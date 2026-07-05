b1d48ce5519de85d1e99584ef76c5709cff9e1fb
Ashish Priyadarshi <Ashish Priyadarshi@LAPTOP-TDMDK58Q.mshome.net>
2026-07-05 21:34:42 +0530

    Added TC006 in SmartCare

--- SmartCare/src/test/java/org/vtiger/campaign/TC001_Verifying2UserIsAbletoCreateCampaignWithMandatoryFieldsTest.java
+++ SmartCare/src/test/java/org/vtiger/campaign/TC001_Verifying2UserIsAbletoCreateCampaignWithMandatoryFieldsTest.java
@@ -2,31 +2,16 @@ package org.vtiger.campaign;
 
 import java.io.IOException;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import org.vtiger.businessutility.BaseClass;
-import org.vtiger.genericutility.FileUtility;
-import org.vtiger.genericutility.JavaUtility;
-import org.vtiger.genericutility.WebDriverUtility;
 import org.vtiger.objectrepository.CampaignPage;
 import org.vtiger.objectrepository.CreateCampaign;
 import org.vtiger.objectrepository.HomePage;
-import org.vtiger.objectrepository.LoginPage;
-import org.vtiger.objectrepository.LoginPageVtiger;
 
 @Listeners (org.vtiger.genericutility.ListenerUtility.class)
 
